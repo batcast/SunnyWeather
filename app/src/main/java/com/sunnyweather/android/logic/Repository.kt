db17bc46bde0c46ef28ef60d82e0a60cf4f4e885
wkai <batcast@qq.com>
2020-08-09 09:09:14 +0800

    Repository运用高阶函数fire

--- app/src/main/java/com/sunnyweather/android/logic/Repository.kt
+++ app/src/main/java/com/sunnyweather/android/logic/Repository.kt
@@ -1,17 +1,15 @@
 package com.sunnyweather.android.logic
 
 import androidx.lifecycle.liveData
-import com.sunnyweather.android.logic.model.NowResponse
-import com.sunnyweather.android.logic.model.PlaceResponse
 import com.sunnyweather.android.logic.model.Weather
 import com.sunnyweather.android.logic.network.SunnyWeatherNetwork
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
+import kotlin.coroutines.CoroutineContext
 
 object Repository {
-    fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
-        val result = try {
+    fun searchPlaces(query: String) = fire(Dispatchers.IO) {
             val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
             if (placeResponse.status == "200") {
                 val locations = placeResponse.location
@@ -19,14 +17,9 @@ object Repository {
             } else {
                 Result.failure(RuntimeException("response status is ${placeResponse.status}"))
             }
-        } catch (e: Exception) {
-            Result.failure<List<PlaceResponse.Place>>(e)
-        }
-        emit(result)
     }
 
-    fun refreshWeather(location: String) = liveData(Dispatchers.IO) {
-        val result = try {
+    fun refreshWeather(location: String) = fire(Dispatchers.IO) {
             coroutineScope {
                 val deferredNow = async {
                     SunnyWeatherNetwork.getNowWeather(location)
@@ -36,17 +29,27 @@ object Repository {
                 }
                 val nowResponse = deferredNow.await()
                 val dailyResponse = deferredDaily.await()
-                if (nowResponse.code == "200" && dailyResponse.code == "200"){
-                    val weather = Weather(nowResponse.now,dailyResponse.daily)
+                if (nowResponse.code == "200" && dailyResponse.code == "200") {
+                    val weather = Weather(nowResponse.now, dailyResponse.daily)
                     Result.success(weather)
-                }else{
-                    Result.failure(RuntimeException("now response code is ${nowResponse.code}" +
-                            "daily response code is ${dailyResponse.code}"))
+                } else {
+                    Result.failure(
+                        RuntimeException(
+                            "now response code is ${nowResponse.code}" +
+                                    "daily response code is ${dailyResponse.code}"
+                        )
+                    )
                 }
             }
-        }catch (e:Exception){
-            Result.failure<Weather>(e)
         }
-        emit(result)
-    }
+
+    private fun <T> fire(context: CoroutineContext, block: suspend () -> Result<T>) =
+        liveData<Result<T>>(context) {
+            val result = try {
+                block()
+            } catch (e: Exception) {
+                Result.failure<T>(e)
+            }
+            emit(result)
+        }
 }
